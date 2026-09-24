c44c6f908b4c9e2a44fbe06b71d560fce987d7f9
eiddym <adito34@hotmail.com>
2026-09-23 23:59:55 -0400

    style: aplicar paleta Azul Galáctico / Dark Slate a headers, navbars y botones; conservar indicador minimalista de hoja superior derecho

--- plantillas-aprobacion-frontend/src/app/index.theme.js
+++ plantillas-aprobacion-frontend/src/app/index.theme.js
@@ -9,8 +9,8 @@
     function ThemeConfig($mdThemingProvider) {
         
         $mdThemingProvider.theme('default')
-                .primaryPalette('teal') // red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green, lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey
-                .accentPalette('orange')
+                .primaryPalette('light-blue')
+                .accentPalette('cyan');
                
     }
 
