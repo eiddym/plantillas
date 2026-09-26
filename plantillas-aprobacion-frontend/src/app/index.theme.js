3ce09914415c57cd021665720b5b2266db74dacf
root <root@srv1745060.hstgr.cloud>
2026-09-26 02:06:34 +0000

    style(theme): implementar sistema de diseño global basado en paleta Petrol Noche y Cyan Eléctrico de Archivo

--- plantillas-aprobacion-frontend/src/app/index.theme.js
+++ plantillas-aprobacion-frontend/src/app/index.theme.js
@@ -7,11 +7,20 @@
 
     /** @ngInject */
     function ThemeConfig($mdThemingProvider) {
-        
         $mdThemingProvider.theme('default')
-                .primaryPalette('blue-grey')
-                .accentPalette('cyan');
-               
+                .primaryPalette('blue-grey', {
+                  'default': '900',
+                  'hue-1': '800',
+                  'hue-2': '700',
+                  'hue-3': '600'
+                })
+                .accentPalette('cyan', {
+                  'default': 'A400',
+                  'hue-1': 'A200',
+                  'hue-2': 'A700'
+                })
+                .warnPalette('red');
     }
 
 })();
+
